07aa78664e4f296248e0cb575b6ef080d6a8d2ad
jdranpariya <jdpatel.code@gmail.com>
2021-01-06 14:49:37 +0530

    setup.py modified

--- django-apps/django-polls-jd/setup.py
+++ django-apps/django-polls-jd/setup.py
@@ -8,5 +8,15 @@ setup(
     author='JDRanpariya',
     author_email='jdpatel.code@gmail.com',
     include_package_data=True,
-    url=
+    url='',
+    packeages=find_packages(),
+    classifiers=[
+        'Develpement Status :: 1 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python'
+    ],
+
 )
