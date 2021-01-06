d2b34ac780e766f489b41e9c81171b5b4f6803a3
jdranpariya <jdpatel.code@gmail.com>
2021-01-06 14:39:23 +0530

    setup created

---
+++ django-apps/django-polls-jd/polls/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.views import generic
+from django.urls import reverse
+from .models import Question, Choice
+# Create your views here.
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')
+
+
+class DetailView(generic.DetailView):  # we can use get_object_or_4o4()
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'polls/result.html'
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:result', args=(question.id,)))
---
+++ django-apps/django-polls-jd/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+import polls
+
+setup(
+    name='django-polls-jd',
+    version=polls.__version__,
+    description='Django app to create Web-based polls.',
+    author='JDRanpariya',
+    author_email='jdpatel.code@gmail.com',
+    include_package_data=True,
+    url=
+)
