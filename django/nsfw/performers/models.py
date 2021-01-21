b6d6135eece5ec1e5cb430ce20839db96ed7ab0d
jdranpariya <jdpatel.code@gmail.com>
2021-01-21 17:41:04 +0530

    model created for performers

---
+++ django/nsfw/performers/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.utils.translation import gettext as _
+
+# Create your models here.
+
+
+class Performer(models.Model):
+
+    class Gender(models.TextChoices):
+        MALE = 'M', _('MALE')
+        FEMALE = 'F', _('FEMALE')
+        TRANS_M = 'TM', _('TRANS_M')
+        TRANS_F = 'TF', _('TRANS_F')
+        INTERSEX = 'IS', _('INTERSEX')
+
+    name = models.CharField("Name", max_length=50)
+    url = models.URLField()
+    gender = models.CharField(max_length=8,
+                              choices=Gender.choices,
+                              default=Gender.FEMALE)
+
+    aliases = models.CharField("aliases", max_length=150)
+    favorite = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
