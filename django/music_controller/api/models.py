a75b84cc6ac0533819a21a90bb0abce7002fc9c8
jdranpariya <jdpatel.code@gmail.com>
2021-01-21 09:52:26 +0530

    added serializer and api

---
+++ django/music_controller/api/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+import random
+import string
+
+# Create your models here.
+
+def generate_unique_code():
+    len = 6
+
+    while True:
+         code = ''.join(random.choices(string.ascii_uppercase, k=len))
+         if Room.objects.filter(code=code).count() == 0:
+             break
+    return code
+
+
+class Room(models.Model):
+    code = models.CharField(max_length=50, default=" ", unique=True)
+    host = models.CharField(max_length=50, default=" ", unique=True)
+    guest_can_pause = models.BooleanField(null=False, default=False)
+    created_at = models.DateField(auto_now_add=True)
+    votes_to_skip = models.IntegerField(null=False, default=1)
+    
---
+++ django/music_controller/api/urls.py
@@ -0,0 +1,7 @@
+
+from django.urls import path, include
+from .views import RoomView 
+
+urlpatterns = [
+    path('', RoomView.as_view()),
+]
